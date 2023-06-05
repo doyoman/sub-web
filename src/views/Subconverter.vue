ff7a5d82993969ca1801bcf0e3305e5f607da159
doyoman <63859866+doyoman@users.noreply.github.com>
2023-06-05 11:13:05 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -298,7 +298,7 @@ export default {
         },
         backendOptions: [
         { 
-            value: "https://sub.doyokey.app/sub?" 
+            value: "https://sub.doyokey.it.eu.org/sub?" 
         },
         {
             value: "https://sub.xeton.dev/sub?"
